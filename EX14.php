8256c587ae026df7142795eba34095dbd8a36ea3
unknown <22yn0114@jec.ac.jp>
2023-06-20 20:33:33 +0900

    kadai

--- EX14.php
+++ EX14.php
@@ -1,26 +1,2 @@
-<<<<<<< HEAD
 <?php
     // 入力された底辺と高さを受け取り、三角形の面積を計算する。
-    $base = $_GET["base"];
-    $height = $_GET["height"];
-
-    print "底辺は{$base}<br>";
-    print "高さは{$height}<br>";
-
-    $area = ($base * $height) /2;
-
-    print "三角形の面積は{$area}<br>";
-=======
-<?php
-    // 入力された底辺と高さを受け取り、三角形の面積を計算する。
-    $base = $_GET["base"];
-    $height = $_GET["height"];
-
-    print "底辺は{$base}<br>";
-    print "高さは{$height}<br>";
-
-    $area = ($base * $height) /2;
-
-    print "三角形の面積は{$area}<br>";
->>>>>>> ecc475b6b492e9b14e290fb13340f971e981da2c
-?>
--- EX15.php
+++ EX15.php
@@ -1,86 +1,22 @@
-<<<<<<< HEAD
 <?php
-// 入力された値を受け取り、計算結果を表示する
-$mag = "";
+    // 入力された値を受け取り、計算結果を表示する
 
-if (isset($_GET["number1"]) && $name = $_GET["number1"] && isset($_GET["number2"]) && $name = $_GET["number2"] && isset($radio)){
-    $number1 = $_GET["number1"];
-$number2 = $_GET["number2"];
-$arithmetic = $_GET["arithmetic"];
-switch ($arithmetic) {
-    case 'add':
-        $msg = $number1 + $number2;
-        break;
-    case 'sub':
-        $msg = $number1 - $number2;
-        break;
-        $msg = $number1 * $number2;
-        break;
-        $msg = $number1 / $number2;
-        break;
-        $msg = $number1 % $number2;
-        break;
-    }
-}else{
-    $msg = "入力内容が間違っています!!";
-}
+
+
 
 
 ?>
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
     <meta charset="utf-8">
     <title>EX15　yyJNccnn　四則演算の入力フォーム(受信)</title>
 </head>
-
 <body>
-<p><?php echo $msg?></p>
-<a href="./EX15.php">入力フォームへ</a>
+
+
+
+
+
 </body>
-
-=======
-<?php
-// 入力された値を受け取り、計算結果を表示する
-$mag = "";
-
-if (isset($_GET["number1"]) && $name = $_GET["number1"] && isset($_GET["number2"]) && $name = $_GET["number2"] && isset($radio)){
-    $number1 = $_GET["number1"];
-$number2 = $_GET["number2"];
-$arithmetic = $_GET["arithmetic"];
-switch ($arithmetic) {
-    case 'add':
-        $msg = $number1 + $number2;
-        break;
-    case 'sub':
-        $msg = $number1 - $number2;
-        break;
-        $msg = $number1 * $number2;
-        break;
-        $msg = $number1 / $number2;
-        break;
-        $msg = $number1 % $number2;
-        break;
-    }
-}else{
-    $msg = "入力内容が間違っています!!";
-}
-
-
-?>
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="utf-8">
-    <title>EX15　yyJNccnn　四則演算の入力フォーム(受信)</title>
-</head>
-
-<body>
-<p><?php echo $msg?></p>
-<a href="./EX15.php">入力フォームへ</a>
-</body>
-
->>>>>>> ecc475b6b492e9b14e290fb13340f971e981da2c
 </html>
